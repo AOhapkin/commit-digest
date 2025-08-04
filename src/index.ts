0cef39077c5c5c9cc450ee743d896ce7ad31104f
AOhapkin <alexeyyago@gmail.com>
2025-08-04 12:50:51 +0300

    Initialize project with TypeScript configuration, package.json, and basic structure for commit digest tool

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { simpleGit, SimpleGit } from "simple-git";
+
+console.log("🚀 Commit Digest - Weekly Report Generator");
+console.log("Initializing...");
+
+// TODO: Добавить основную логику для генерации отчетов
